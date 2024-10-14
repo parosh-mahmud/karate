2ddd68d5fe28448ea92a77fd1b65bcc8247ff464
ashifbabu <md.asif.dev@gmail.com>
2024-10-14 21:41:00 +0600

    firebase removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import "../styles/globals.css";
 import DefaultLayout from "../components/layouts/DefaultLayout";
-import AnalyticsClientOnly from "../components/AnalyticsClientOnly"; // Import the client-only analytics component
 
 function MyApp({ Component, pageProps }) {
   return (
     <DefaultLayout>
       {/* Add AnalyticsClientOnly here to initialize Firebase Analytics on the client side */}
-      <AnalyticsClientOnly />
+
       <Component {...pageProps} />
     </DefaultLayout>
   );
