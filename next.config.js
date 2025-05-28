55dd663648f663385cfca3aaec74ef2effc2526d
parosh-mahmud <prparosh@gmail.com>
2025-05-29 03:21:03 +0600

    jsconfig added

--- next.config.js
+++ next.config.js
@@ -34,34 +34,19 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Allow optimized images from Cloudinary
   images: {
-    domains: [
-      "storage.googleapis.com", // For Firebase Storage
-      "firebasestorage.googleapis.com",
-    ],
+    domains: ["storage.googleapis.com", "firebasestorage.googleapis.com"],
     unoptimized: true,
-    minimumCacheTTL: 60, // Cache images for 60 seconds
   },
-
-  // Internationalization settings
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-
-  // Use standalone output
   output: "standalone",
-
-  // Performance optimizations
-  swcMinify: true, // Use SWC for minification (faster than Terser)
-  reactStrictMode: true, // Enable React strict mode
-
-  // Custom webpack tweaks
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": __dirname,
+      "@": ".",
     };
     return config;
   },
